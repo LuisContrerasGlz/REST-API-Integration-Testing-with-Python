fc170a7105b17a642ebc8e7c55c0b01fa06e1acf
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-24 14:03:57 -0600

    API

--- test_todo_api.py
+++ test_todo_api.py
@@ -3,4 +3,10 @@ import requests
 ENDPOINT = "https://todo.pixegami.io/"
 
 response = requests.get(ENDPOINT)
-print(response)
+print(response) 
+
+data = response.json()
+print(data)
+
+status_code = response.status_code
+print(status_code)
