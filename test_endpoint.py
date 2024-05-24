46b38f5c3d32476c73de1386813261ff22664d6f
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-24 14:53:00 -0600

    API

--- test_endpoint.py
+++ test_endpoint.py
@@ -8,3 +8,12 @@ def test_can_call_endpoint():
     response = requests.get(ENDPOINT)
     assert response.status_code == 200
 
+def test_can_create_tasl():
+    payload = {
+        "content": "my test content",
+        "user_id": "test_user",
+        "is_done": False
+    }
+    response = requests.put(ENDPOINT + "/create-task", json=payload)
+    assert response.status_code == 200
+
