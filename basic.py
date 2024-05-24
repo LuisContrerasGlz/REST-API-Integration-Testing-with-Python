02ae85bb1a79cfe16bd4454a865b2d994fb094a2
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-24 14:43:33 -0600

    API

--- basic.py
+++ basic.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 ENDPOINT = "https://todo.pixegami.io/"
---
+++ test_endpoint.py
@@ -0,0 +1,10 @@
+import pytest
+import requests
+
+ENDPOINT = "https://todo.pixegami.io/"
+
+
+def test_can_call_endpoint():
+    response = requests.get(ENDPOINT)
+    assert response.status_code == 200
+
